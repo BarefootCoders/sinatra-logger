7a6e9e26810d078d5a0d428593e225a8717d15d3
Yehya Abouelnaga <yehya.abouelnaga@minodes.com>
2016-07-12 18:43:10 +0200

    Move to version 0.2.0 (updated the injection to be neater)

--- lib/minodes/sinatra/logger.rb
+++ lib/minodes/sinatra/logger.rb
@@ -15,56 +15,29 @@ module Minodes
     end
 
     module Logger
-      class << self
-        attr_accessor :configuration
-        attr_accessor :appenders_assigned
-      end
-
-      def self.configure
-        self.configuration ||= Configuration.new
-        yield(self.configuration)
-
-        if configuration.file_name.nil?
-          raise "Minodes::Sinatra::Logger -- File name is not specified. Please, set `file_name` in the configuration block!"
-        end
-
-        if configuration.app.nil?
-          raise "Minodes::Sinatra::Logger -- App is not specified. Please, set `app` in the configuration block!"
-        end
-
-        ::Sinatra::Base.class_eval do
-          before do
-            unless Minodes::Sinatra::Logger.appenders_assigned      # Assign appenders only once in the request lifetime
-              Minodes::Sinatra::Logger.appenders_assigned = true    # There will be as many requests as there're applications in the middleware stack.
-                                                                    # For example, if you create multiple Sinatra::Application (and use then inside each other, this use case will pop up)
-
-              env["rack.errors"] = Minodes::Sinatra::ErrorLogger.new
-              env["rack.logger"] = ::SemanticLogger[self.class.name]
-
-              # Re-assign the appenders (on every request)
-              ::SemanticLogger.default_level = configuration.log_level
-              ::SemanticLogger.appenders.each { |a| ::SemanticLogger.remove_appender(a) }
-              ::SemanticLogger.add_appender(file_name: configuration.file_name, formatter: :color)
-            end
+      ::Sinatra::Base.class_eval do
+        def self.logger(config)
+          if config[:filename].nil?
+            raise "Minodes::Sinatra::Logger -- File name is not specified. Please, set `file_name` in the configuration block!"
           end
-        end
 
-        # It's enough to assign the common logger to the parent app! (even if there're multiple modular/nested apps included)
-        configuration.app.configure do
+          config[:level] ||= :trace
+
+          ::SemanticLogger.default_level = config[:level]
+          ::SemanticLogger.appenders.each { |a| ::SemanticLogger.remove_appender(a) }
+          ::SemanticLogger.add_appender(file_name: config[:filename], formatter: :color)
+
           set :logging, true
           use ::Rack::CommonLogger, ::SemanticLogger["Access"]
-        end
-      end
 
-      class Configuration
-        attr_accessor :log_level
-        attr_accessor :file_name
-        attr_accessor :app
+          ::Sinatra::Base.before do
+            ::SemanticLogger.default_level = config[:level]
+            ::SemanticLogger.appenders.each { |a| ::SemanticLogger.remove_appender(a) }
+            ::SemanticLogger.add_appender(file_name: config[:filename], formatter: :color)
 
-        def initialize
-          @log_level = :warn
-          @file_name = nil
-          @app = nil
+            env["rack.errors"] = ::Minodes::Sinatra::ErrorLogger.new
+            env["rack.logger"] = ::SemanticLogger[self.class.name]
+          end
         end
       end
     end
--- lib/minodes/sinatra/logger/version.rb
+++ lib/minodes/sinatra/logger/version.rb
@@ -1,7 +1,7 @@
 module Minodes
   module Sinatra
     module Logger
-      VERSION = "0.1.1"
+      VERSION = "0.2.0"
     end
   end
 end
